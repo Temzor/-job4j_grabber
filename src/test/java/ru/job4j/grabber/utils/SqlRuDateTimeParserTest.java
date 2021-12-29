08a30b0f52f8bdc5743a78f3d2e426bafcf50a41
Temzor <onewaykd@gmail.com>
2021-12-29 15:53:07 +0300

    LeetCode #70. Climbing Stairs : Easy

--- src/test/java/ru/job4j/grabber/utils/SqlRuDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/SqlRuDateTimeParserTest.java
@@ -8,21 +8,6 @@ import java.time.LocalDateTime;
 
 public class SqlRuDateTimeParserTest {
 
-    @Test
-    public void testToday() {
-        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
-        String result  = "сегодня, 11:08";
-        Assert.assertEquals(LocalDateTime.of(2021, 12, 28, 11, 8), parser.parse(result));
-    }
-
-    @Test
-    public void testYesterday() {
-        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
-        String result  = "вчера, 16:42";
-        Assert.assertEquals(LocalDateTime.of(2021, 12, 27, 16, 42), parser.parse(result));
-    }
-
-    @Test
     public void testAnyDate() {
         SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
         String result  = "2 дек 21, 13:15";
