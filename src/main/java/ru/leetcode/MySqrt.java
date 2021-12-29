9041fdde6c6842e6300d670cdaf4e3845c777d1e
Temzor <onewaykd@gmail.com>
2021-12-29 14:03:57 +0300

    LeetCode #69. Sqrt(x) : Easy

--- src/main/java/ru/leetcode/MySqrt.java
+++ src/main/java/ru/leetcode/MySqrt.java
@@ -2,6 +2,9 @@ package ru.leetcode;
 
 public class MySqrt {
     public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+        long r = x;
+        while (r*r > x)
+            r = (r + x/r) / 2;
+        return (int) r;
     }
 }
