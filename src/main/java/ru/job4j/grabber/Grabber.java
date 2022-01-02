d9dcdb368d9d00893184667dd2d3d13bf6092767
Капустин Дмитрий <onewaykd@gmail.com>
2022-01-02 12:42:29 +0300

    6. Grabber. [#289477]

---
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -0,0 +1,86 @@
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.html.SqlRuParse;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private final Properties configPostgreSql = new Properties();
+    private final Properties configRabbitQuartz = new Properties();
+
+    public Store store() {
+        return new PsqlStore(configPostgreSql);
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void configPostgreSql() throws IOException {
+        try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream(
+                "app.properties")) {
+            configPostgreSql.load(in);
+        }
+    }
+
+    public void configRabbitQuartz() throws IOException {
+        try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream(
+                "rabbit.properties")) {
+            configRabbitQuartz.load(in);
+        }
+    }
+
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(configRabbitQuartz.getProperty("time")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            try {
+                parse.list("http://www.sql.ru/forum/job-offers/")
+                        .forEach(store::save);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Grabber grab = new Grabber();
+        grab.configPostgreSql();
+        grab.configRabbitQuartz();
+        Scheduler scheduler = grab.scheduler();
+        Store store = grab.store();
+        grab.init(new SqlRuParse(new SqlRuDateTimeParser()), store, scheduler);
+    }
+}
