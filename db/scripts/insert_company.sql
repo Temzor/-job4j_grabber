652f370533b85720e3d710ea75c12334119df227
Капустин Дмитрий <onewaykd@gmail.com>
2022-01-03 17:41:07 +0300

    2. SQL Select by company [#2760]

---
+++ db/scripts/insert_company.sql
@@ -0,0 +1,15 @@
+INSERT INTO company VALUES (1, 'CSI');
+INSERT INTO company VALUES (2, 'Job4');
+INSERT INTO company VALUES (3, 'Mail.ru');
+INSERT INTO company VALUES (4, 'Yandex');
+INSERT INTO company VALUES (5, 'Epam');
+
+INSERT INTO person VALUES (1, 'Dmitrii', 1);
+INSERT INTO person VALUES (2, 'Petr', 2);
+INSERT INTO person VALUES (3, 'Alexye', 3);
+INSERT INTO person VALUES (4, 'Marina', 4);
+INSERT INTO person VALUES (5, 'Tanya', 5);
+INSERT INTO person VALUES (6, 'Oraz', 3);
+INSERT INTO person VALUES (7, 'Nurlan', 3);
+INSERT INTO person VALUES (8, 'Kostya', 2);
+INSERT INTO person VALUES (9, 'Inokentii', 2);
---
+++ db/scripts/select_company.sql
@@ -0,0 +1,17 @@
+SELECT pr.name "Name employe", cm.name "Name company"
+FROM person pr
+INNER JOIN company cm ON cm.id = pr.company_id 
+WHERE cm.id != 5
+ORDER BY cm.id;
+
+SELECT cm.name "Name company", COUNT(cm.name) "Count employe"
+FROM company AS cm
+INNER JOIN person pr ON pr.company_id = cm.id
+GROUP BY cm.name
+HAVING COUNT(cm.name) = (
+		SELECT COUNT(company_id)
+		FROM person
+		GROUP BY company_id
+		LIMIT 1
+);
+
